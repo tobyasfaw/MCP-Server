164099193d72b02811f2932529fc5bed533dba6f
tobyasfaw <tasfaw@scu.edu>
2025-10-22 23:23:22 -0700

    Delete mcp-server directory

--- mcp-server/src/config.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const config = {
-  googleApiKey: process.env.GOOGLE_API_KEY!,
-  notionToken: process.env.NOTION_API_KEY!,
-  githubToken: process.env.GITHUB_TOKEN!,
-};
--- mcp-server/src/tools/google.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import { config } from '../config';
-
-export async function listGoogleDocs() {
-  const url = `https://www.googleapis.com/drive/v3/files?q=mimeType='application/vnd.google-apps.document'&key=${config.googleApiKey}`;
-  const res = await axios.get(url);
-  return res.data.files;
-}
